24b37f5efd4c49c760b8bb6d750588f6753894b6
Isaiahw18 <72327223+Isaiahw18@users.noreply.github.com>
2022-08-24 13:25:33 -0400

    Update LInRegmodel.py

--- LInRegmodel.py
+++ LInRegmodel.py
@@ -40,7 +40,7 @@ X, y
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X, X, y, y = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 
 # In[11]:
@@ -48,7 +48,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X, y)
 
 
 # In[12]:
@@ -60,15 +60,15 @@ regressor
 # In[16]:
 
 
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X)
 y_pred
 
 
 # In[17]:
 
 
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
@@ -84,18 +84,10 @@ plt.show()
 # In[19]:
 
 
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X, color = 'blue')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
-
-
-# In[ ]:
-st.title('Salary Estimate Predictor')
-st.header('Years Expericne:')
-st.number_input("Enter Int Value:")
-salary = regressor.predict('Salary')
-st.success()
 
